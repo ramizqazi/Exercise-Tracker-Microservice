460461dd6818188da36d137e5176ec0a5e0c5931
ramizqazi <ramizqazi03@gmail.com>
2023-12-15 00:33:46 +0500

    deployed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const moment = require('moment');
@@ -20,11 +20,11 @@ let UserSchema = new Schema({
 
 let User = mongoose.model("Users", UserSchema);
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 // ADD USER
@@ -32,31 +32,33 @@ app.post('/api/users', function (req, res) {
   const username = req.body.username;
 
   if (!username) {
-    res.json({ error: 'Invalid username' })
-    return
+    res.json({ error: 'Invalid username' });
+    return;
   }
 
-  const user = new User({ username, log: [] })
+  const user = new User({ username, log: [] });
 
   user.save().then((data) => {
-    res.json({ username: data.username, _id: data._id })
+    res.json({ username: data.username, _id: data._id });
   }).catch(err => {
     console.log(err);
-  })
-})
+    res.json({ error: 'Error creating user' });
+  });
+});
 
 // GET USERS
 app.get('/api/users', function (req, res) {
   const users = [];
 
   User.find().then((data) => {
-    data.forEach((res) => users.push({ _id: res._id, username: res.username }))
+    data.forEach((user) => users.push({ _id: user._id, username: user.username }));
   }).catch(err => {
     console.log(err);
+    res.json({ error: 'Error fetching users' });
   }).finally(() => {
-    res.json(users)
-  })
-})
+    res.json(users);
+  });
+});
 
 // GET USER LOGS
 app.get('/api/users/:_id/logs', function (req, res) {
@@ -73,17 +75,24 @@ app.get('/api/users/:_id/logs', function (req, res) {
 
       // Filter logs based on 'from' and 'to' dates
       if (from) {
-        logs = logs.filter((log) => moment(log.date, 'YYYY-MM-DD').isSameOrAfter(moment(from), 'dates'));
+        logs = logs.filter((log) => moment(log.date, 'YYYY-MM-DD').isSameOrAfter(moment(from), 'day'));
       }
 
       if (to) {
-        logs = logs.filter((log) => moment(log.date, 'YYYY-MM-DD').isSameOrBefore(moment(to), 'dates'));
+        logs = logs.filter((log) => moment(log.date, 'YYYY-MM-DD').isSameOrBefore(moment(to), 'day'));
       }
 
       // Limit the number of logs if 'limit' is provided
       if (limit) {
         logs = logs.slice(0, parseInt(limit, 10));
       }
+
+      // Format date in the log array
+      logs = logs.map((log) => ({
+        description: log.description,
+        duration: log.duration,
+        date: new Date(log.date).toDateString(),
+      }));
 
       res.json({
         _id: user._id,
@@ -113,7 +122,6 @@ app.post('/api/users/:_id/exercises', function (req, res) {
     {
       $push: {
         log: {
-          _id,
           description: description,
           duration: Number(duration),
           date: date ? new Date(date).toDateString() : new Date().toDateString(),
@@ -130,10 +138,12 @@ app.post('/api/users/:_id/exercises', function (req, res) {
       const log = user.log[user.log.length - 1];
 
       res.json({
+        _id: user._id,
         username: user.username,
-        date: log.date,
-        duration: log.duration,
         description: log.description,
+        duration: log.duration,
+        date: log.date,
+        _exerciseId: log._id,
       });
     })
     .catch((e) => {
@@ -142,5 +152,5 @@ app.post('/api/users/:_id/exercises', function (req, res) {
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
